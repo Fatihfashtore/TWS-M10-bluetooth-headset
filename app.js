eaa9d348eb0fef371b55b8478ea642a6395e45dd
FatihStore <8402629+Fatihfashtore@users.noreply.github.com>
2025-06-11 11:34:00 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 
-document.getElementById("pesanBtn").addEventListener("click", function () {
-  fbq('track', 'ViewContent');
-  fbq('track', 'Purchase', { value: 174000, currency: 'IDR' });
-
-  var nama = document.getElementById("nama").value;
-  var wa = document.getElementById("wa").value;
-  var alamat = document.getElementById("alamat").value;
-  var varian = document.getElementById("varian").value;
-  var pembayaran = document.getElementById("pembayaran").value;
-
-  var pesan = 'Halo, saya mau pesan headset:\nNama: ' + nama + '\nNo WA: ' + wa + '\nAlamat: ' + alamat + '\nVarian: ' + varian + '\nPembayaran: ' + pembayaran;
-  var url = 'https://wa.me/6281316660739?text=' + encodeURIComponent(pesan);
-  window.open(url, '_blank');
+document.getElementById('orderForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nama = this.nama.value;
+    const whatsapp = this.whatsapp.value;
+    const alamat = this.alamat.value;
+    const pembayaran = this.pembayaran.value;
+    const message = `Halo, saya ingin memesan Bluetooth Headset:%0ANama: ${nama}%0ANo Whatsapp: ${whatsapp}%0AAlamat: ${alamat}%0AMetode Pembayaran: ${pembayaran}`;
+    window.open(`https://wa.me/6281316660739?text=${message}`, '_blank');
 });
